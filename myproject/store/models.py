94e0a7348b1f4f55d42b6cf2c1fe91b9a5dc7c5c
Eren Küçükkurt <eren.kucukkurt@sabanciuniv.edu>
2024-11-07 17:15:33 +0300

    Added image part to the product model

--- myproject/store/models.py
+++ myproject/store/models.py
@@ -10,6 +10,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     warranty_status = models.CharField(max_length=100, default="No Warranty")  # E.g., "1 year", "2 years"
     distributor_info = models.CharField(max_length=255, null=True, blank=True, default="Unknown Distributor")
+    image = models.ImageField(upload_to="products/", null=True, blank=True)
 
     def __str__(self):
         return f"{self.name} ({self.model})"
